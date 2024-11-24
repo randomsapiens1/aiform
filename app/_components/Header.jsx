d69bef2e1e796a1154ebccb267bd4f1dc90b9706
randomsapiens1 <imsapiens.ctf@gmail.com>
2024-11-25 01:47:13 +0600

    fixed-clerk(File structure Issue)

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,6 +1,5 @@
  import React from 'react'
  import Image from 'next/image'
- import { Button } from '@/components/ui/button'
  
  function Header() {
    return (
@@ -8,7 +7,6 @@
      <div className='p-5 border-b shadow-sm'>
         <div className='flex items-center justify-between'>
             <Image src={'/logo.svg'} width={50} height={30} alt='logo'/>
-            <Button>Get Started!</Button>
         </div>
      </div>
    )
